e0e712b9a9a28399050802c0eae4ccf54aeac1b9
hasancse18 <hasan7779296@gmail.com>
2024-03-15 10:10:32 +0600

    238 product of array except itself

---
+++ 238_product_of_array_exceptitself.cpp
@@ -0,0 +1,16 @@
+ vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>vl(n,1),vr(n,1);
+        int i,j;
+        for(i=1;i<n;i++)
+        {
+            j=n-i;
+            vl[i]=vl[i-1]*nums[i-1];
+            vr[j-1]=vr[j]*nums[j];
+        }
+        for(i=0;i<n;i++)
+        {
+            nums[i]=vl[i]*vr[i];
+        }
+        return nums;
+    }
