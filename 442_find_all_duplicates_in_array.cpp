3b15a5806060c581d648dd514a601947b983e82f
hasancse18 <hasan7779296@gmail.com>
2024-03-25 15:50:40 +0600

    442 Find all dupicates in array

---
+++ 442_find_all_duplicates_in_array.cpp
@@ -0,0 +1,14 @@
+vector<int> findDuplicates(vector<int>& nums) {
+       map<int,int>mp;
+       vector<int>ans;
+       for(auto it:nums)
+       {
+        mp[it]++;
+       }
+       for(auto it:mp)
+       {
+        if(it.second==2)
+            ans.push_back(it.first);
+       }
+       return ans;
+    }
