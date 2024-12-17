e51ec2d17a5dcb71b3ab81890925557e2853be2c
hasancse18 <hasan7779296@gmail.com>
2024-12-17 22:42:34 +0600

    sieve of eratosthence is used to find the number prime exist in given n

---
+++ prime_number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        if(n<2) return 0;
+        
+        vector<bool>prime(n,true);
+        prime[0]=prime[1]=false;
+        for(int i=2;i*i<n;i++)
+        {
+            if(prime[i])
+            {
+                for(int j=i*i;j<n;j+=i)
+                {
+                    prime[j]=false;
+                }
+            }
+        }
+        int c=0;
+        for(auto it: prime)
+        {
+            if(it) c++;
+        }
+        return c;
+    }
+};
